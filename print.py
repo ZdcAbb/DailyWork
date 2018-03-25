e1f4e3f089ce2f0fe78c627822b5c036c22de56c
ZdcAbb <32324340+ZdcAbb@users.noreply.github.com>
2018-03-25 09:46:55 +0800

    Update print.py

--- print.py
+++ print.py
@@ -1,10 +1,33 @@
+import urllib
+import json
+
 print "hello world"
 #coding=utf-8
 
-# ¹¦ÄÜ
+# Â¹Â¦Ã„Ãœ
 def func(x):
     return x + 2
 
-# ²âÊÔÓÃÀý
+# Â²Ã¢ÃŠÃ”Ã“ÃƒÃ€Ã½
 def test_answer():
     assert func(3) == 5
+    
+    
+print "hello world"
+import urllib
+import json
+
+def get_dic(url):
+    page = urllib.urlopen(url)
+    html = page.read()
+    page.close()
+    dic=json.loads(html)
+    return dic
+
+dic = get_dic("http://www.weather.com.cn/data/cityinfo/101010100.html")
+
+print dic['weatherinfo']['city']
+print dic['weatherinfo']['ptime']
+print dic['weatherinfo']['temp1']
+print dic['weatherinfo']['temp2']
+print dic['weatherinfo']['weather']
