89753a704374bab295c70802a355f60bd48aa053
daocheng <daocheng.zhang@cn.abb.com>
2020-06-29 16:58:34 +0800

    test

--- print.py
+++ print.py
@@ -14,20 +14,4 @@ def test_answer():
     
     
 print "hello world"
-import urllib
-import json
 
-def get_dic(url):
-    page = urllib.urlopen(url)
-    html = page.read()
-    page.close()
-    dic=json.loads(html)
-    return dic
-
-dic = get_dic("http://www.weather.com.cn/data/cityinfo/101010100.html")
-
-print dic['weatherinfo']['city']
-print dic['weatherinfo']['ptime']
-print dic['weatherinfo']['temp1']
-print dic['weatherinfo']['temp2']
-print dic['weatherinfo']['weather']
